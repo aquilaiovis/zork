7a18188bd66b8dc804c0cd9892dbd75350242fac
Scheissegal <Lukas.Wipf@gmx.ch>
2018-12-05 14:50:23 +0100

    added first test

--- src/test/java/ch/bbw/zork/ParserTest.java
+++ src/test/java/ch/bbw/zork/ParserTest.java
@@ -2,10 +2,18 @@ package ch.bbw.zork;
 
 import org.junit.Test;
 
+import static org.junit.Assert.assertEquals;
+
+import java.io.ByteArrayInputStream;
+
+
 public class ParserTest {
+
+    private Parser parser;
 
     @Test
     public void testParser() {
-
+        parser = new Parser(new ByteArrayInputStream("go west".getBytes()));
+        assertEquals("The commands should be the same.", new Command(new CommandWords(), "go", "west"), parser.getCommand());
     }
 }
