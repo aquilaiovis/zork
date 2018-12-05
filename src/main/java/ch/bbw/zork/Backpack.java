2d19f96d62205de674a79a3303a2a3874b549a0b
aquilaiovis <hermann.eichhorn@hotmail.com>
2018-12-05 14:13:30 +0100

    Improve Game.processCommand()

--- src/main/java/ch/bbw/zork/Backpack.java
+++ src/main/java/ch/bbw/zork/Backpack.java
@@ -17,9 +17,34 @@ public class Backpack
         {
             if(item.getName().equals(secondCommand))
             {
+                System.out.println(item.getName() + " was successfully picked up.");
                 return item;
             }
         }
+        System.out.println("This item is not in the current room.");
         return null;
+    }
+
+    public void addItem(Item item)
+    {
+        items.add(item);
+    }
+
+    public void removeItem(String secondCommand)
+    {
+        for(Item item: items)
+        {
+            if(item.getName().equals(secondCommand))
+            {
+                items.remove(item);
+                System.out.println(item.getName() + " was successfully removed.");
+            }
+        }
+        System.out.println("This item is not in your backpack.");
+    }
+
+    public ArrayList<Item> getItems()
+    {
+        return items;
     }
 }
