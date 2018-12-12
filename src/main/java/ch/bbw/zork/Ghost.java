abaea942d08fa8086ef39f915506dd9767234000
Scheissegal <Lukas.Wipf@gmx.ch>
2018-12-12 14:17:22 +0100

    filled interact in goodghost

--- src/main/java/ch/bbw/zork/Ghost.java
+++ src/main/java/ch/bbw/zork/Ghost.java
@@ -2,6 +2,16 @@ package ch.bbw.zork;
 
 public abstract class Ghost {
 
+    private Game game;
+
+    public Ghost(Game game) {
+        this.game = game;
+    }
+
     public abstract void interact();
 
+
+    public Game getGame() {
+        return game;
+    }
 }
--- src/main/java/ch/bbw/zork/GoodGhost.java
+++ src/main/java/ch/bbw/zork/GoodGhost.java
@@ -8,6 +8,18 @@ public class GoodGhost extends Ghost {
 
     @Override
     public void interact() {
-        System.out.println("Yo wanna hear a secret... The veil is in the ");
+        System.out.println("Yo wanna hear a secret... The veil is in the " + getVeil() + ". Yo look out for the Bad " +
+                "ghosts. Those snitches wanna kill you!");
+    }
+
+    private String getVeil() {
+        for (Room room:super.getGame().getMap()) {
+            for (Item item:room.getItems()) {
+                if (item.getName().equals("veil")) {
+                    return room.getName();
+                }
+            }
+        }
+        return "backpack";
     }
 }
