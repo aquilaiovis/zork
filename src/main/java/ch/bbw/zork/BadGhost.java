b9531a0be10b2202e5406f627fc1d34936b2bebe
Scheissegal <Lukas.Wipf@gmx.ch>
2018-12-12 13:39:51 +0100

    added over in game

--- src/main/java/ch/bbw/zork/BadGhost.java
+++ src/main/java/ch/bbw/zork/BadGhost.java
@@ -2,8 +2,14 @@ package ch.bbw.zork;
 
 public class BadGhost extends Ghost {
 
+    private Game game;
+
+    public BadGhost(Game game) {
+        this.game = game;
+    }
+
     @Override
     public void interact() {
-
+        game.over();
     }
 }
