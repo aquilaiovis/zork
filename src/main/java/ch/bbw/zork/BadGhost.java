28dacd5889e3553a5472a4f998dbd024c7e09ef2
aquilaiovis <hermann.eichhorn@hotmail.com>
2018-12-12 14:55:14 +0100

    Add new game-over-mechanic: When an evil ghost is in the same room as the player and the player doesn't wear the invisibility cloak, the game ends.

--- src/main/java/ch/bbw/zork/BadGhost.java
+++ src/main/java/ch/bbw/zork/BadGhost.java
@@ -9,6 +9,7 @@ public class BadGhost extends Ghost {
     @Override
     public void interact() {
         if (super.getGame().getBackpack().getItem("cape") == null) {
+            System.out.println("The ghost says: \"You have disturbed my eternal sleep. Now, I shall take your body and take over the wooorld!\"");
             super.getGame().over();
         }
     }
--- src/main/java/ch/bbw/zork/GoodGhost.java
+++ src/main/java/ch/bbw/zork/GoodGhost.java
@@ -8,8 +8,8 @@ public class GoodGhost extends Ghost {
 
     @Override
     public void interact() {
-        System.out.println("Yo wanna hear a secret... The cape is in the " + getCapeRoom() + ". Yo look out for the Bad " +
-                "ghosts. Those snitches wanna kill you if you're not wearing it!");
+        System.out.println("The ghost speaks: \"Yo wanna hear a secret... The invisibility cloak is in the " + getCapeRoom()
+                + ". Yo look out for the bad ghosts. Those snitches wanna kill you if you're not wearing it!\"");
     }
 
     private String getCapeRoom() {
